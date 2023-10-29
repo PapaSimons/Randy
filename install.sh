906bf82493c117a812072204669f37ed95c32c62
PapaSimons <gideonstk@gmail.com>
2023-10-29 21:42:23 +0800

    new nodejs installation

--- install.sh
+++ install.sh
@@ -29,11 +29,16 @@ for i in $INSTALL_PKGS; do
 done
 
 echo "-------------------------------------------"
-echo "######>>> installing nodejs lts"
+echo "######>>> installing nodejs"
 echo "-------------------------------------------"
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - &&\
-apt-get install -y nodejs
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
 
 echo "-------------------------------------------"
 echo "######>>> installing mpv"
