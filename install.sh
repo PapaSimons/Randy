097f0f1b42b11ef841cdd86ef336ddcf4271094d
PapaSimons <gideonstk@gmail.com>
2024-05-22 14:59:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,12 +32,13 @@ echo "-------------------------------------------"
 echo "######>>> installing nodejs"
 echo "-------------------------------------------"
 
-# installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# download and install Node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 22
-# verifies the right Node.js version is in the environment
-node -v # should print `v22.2.0`
+node -v 
+npm -v
 
 echo "-------------------------------------------"
 echo "######>>> installing mpv"
