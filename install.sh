fc28a1ab5f9e48fef1ebf581058e9841bdbecd8c
PapaSimons <gideonstk@gmail.com>
2024-05-22 21:16:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,9 +33,7 @@ echo "######>>> installing nodejs"
 echo "-------------------------------------------"
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/.bashrc
 nvm install 22
 node -v 
 npm -v
