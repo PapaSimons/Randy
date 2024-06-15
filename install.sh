2d4443d7649df5f31fb76d95957cdb55ddb89b03
PapaSimons <gideonstk@gmail.com>
2024-06-15 21:16:38 +0800

    back to older nodejs installation

--- install.sh
+++ install.sh
@@ -32,18 +32,13 @@ echo "-------------------------------------------"
 echo "######>>> installing nodejs"
 echo "-------------------------------------------"
 
-# Function to install NVM
-install_nvm() {
-    echo "Installing NVM..."
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-}
-#install node with nvm
-install_nvm
-node_version=22.3.0
-nvm install $node_version
-nvm use $node_version
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=22
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
 
 echo "-------------------------------------------"
 echo "######>>> installing mpv"
